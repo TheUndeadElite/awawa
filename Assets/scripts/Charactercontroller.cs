f9a97d96aa1907ee98a9ed46f48476aade435192
Almir Bajraktarevic <almir.bajraktarevic@learnet.se>
2023-09-26 10:49:57 +0200

    gravityspeed

--- Assets/scripts/Charactercontroller.cs
+++ Assets/scripts/Charactercontroller.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
+//TODO: make gravity slower when jumping then increasing speed while falling.
 public class charactercontroller: MonoBehaviour
 {
     public float MovementSpeed = 100.0f;
+
+    //gravity
+    public float Gravityspeed = 50.0f;
+    public float GroundLevel = 0.0f;
     void Update()
     {
-        
+        //gravity
+        Vector3 gravityposition = transform.position;
+        gravityposition.y -= Gravityspeed * Time.deltaTime;
+        if(gravityposition.y < GroundLevel) {gravityposition.y = GroundLevel;}
+        transform.position = gravityposition;
+
+
         //Up
         if (Input.GetKey(KeyCode.W)) 
         { 
-            Vector3 characterposition = transform.position;
+            Vector3 characterposition = transform.position; //copy character position
             characterposition.y += MovementSpeed * Time.deltaTime;
-            transform.position = characterposition;
+            transform.position = characterposition; // paste new position
         }
         
         //Left
