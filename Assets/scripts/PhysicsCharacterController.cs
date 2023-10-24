cef56ed5c780df98ec20a2f2c394e0cbf00b0e63
Almir Bajraktarevic <almir.bajraktarevic@learnet.se>
2023-10-24 09:49:15 +0200

    code fixing

--- Assets/scripts/PhysicsCharacterController.cs
+++ Assets/scripts/PhysicsCharacterController.cs
@@ -20,38 +20,44 @@ public class PhysicsCharacterController : MonoBehaviour
     public float JumpMaxHeight = 150.0f;
     [SerializeField]
     private float JumpHeightDelta = 0.0f;
+    private float JumpStartingY = 0.0f;
     //Movement
     public float MovementSpeedPerSecond = 10.0f; //Movement Speed
 
 
-
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && JumpingState == characterstate.Grounded)
+        {
+            JumpingState = characterstate.Jumping; //Set character to jumping
+            JumpHeightDelta = 0.0f; //Restart Counting Jumpdistance
+            JumpStartingY = transform.position.y;
+        }
+    }
 
 
     void FixedUpdate()
     {
         Vector3 characterVelocity = myRigidBody.velocity;
         characterVelocity.x = 0.0f;
-        characterVelocity.y = 0.0f;
 
-        if (true)
+        if(JumpingState == characterstate.Airborne)
         {
-            JumpingState = characterstate.Grounded;
+            if (characterVelocity.x > -1.0f)
+            {
+                JumpingState = characterstate.Airborne;
+            }
         }
-
-        //Up
-        if (Input.GetKey(KeyCode.W) && JumpingState == characterstate.Grounded)
-        {
-            JumpingState = characterstate.Jumping; //Set character to jumping
-            JumpHeightDelta = 0.0f; //Restart Counting Jumpdistance
-        }
+      
 
         if (JumpingState == characterstate.Jumping)
         {
 
-            float totalJumpMovementThisFrame = MovementSpeedPerSecond * JumpSpeedFactor;
-            characterVelocity.y += totalJumpMovementThisFrame;
+            float jumpMovement = MovementSpeedPerSecond * JumpSpeedFactor;
+            characterVelocity.y = jumpMovement;
 
-            JumpHeightDelta += totalJumpMovementThisFrame;
+            JumpHeightDelta += jumpMovement * Time.deltaTime;
+
             if (JumpHeightDelta >= JumpMaxHeight)
             {
                 JumpingState = characterstate.Airborne;
